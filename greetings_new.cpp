040fe27a91d209033b7c8597ff8e50313a8ee622
Mwalili <84922460+Mwalili@users.noreply.github.com>
2021-06-18 20:09:34 +0300

    I have added a new file greetings_new.cpp

---
+++ greetings_new.cpp
@@ -0,0 +1,11 @@
+
+// Greetings from USIU APP STUDENTS
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello Dr Mwalili,Nice to meet you here at GIT";
+	std::cout << "Hello Dr Mwalili,Nice to meet you here at GIT for the Second Time ";
+	std::cout << "Hello Dr Mwalili,Nice to meet you here at GIT for the Third Time ";
+    return 0;
+}
