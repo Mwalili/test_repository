8bde76e5588762ad91c20389be4cc84d0d1da614
Mwalili <84922460+Mwalili@users.noreply.github.com>
2021-06-18 20:03:55 +0300

    New feature added to greetings.cpp

--- greetings.cpp
+++ greetings.cpp
@@ -5,6 +5,7 @@
 
 int main() {
     std::cout << "Hello Dr Mwalili,Nice to meet you here at GIT";
-	 std::cout << "Hello Dr Mwalili,Nice to meet you here at GIT for the Second Time";
+	std::cout << "Hello Dr Mwalili,Nice to meet you here at GIT for the Second Time ";
+	std::cout << "Hello Dr Mwalili,Nice to meet you here at GIT for the Third Time ";
     return 0;
 }
